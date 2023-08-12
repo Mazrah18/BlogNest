31db6cabd63ade60a11865c5c03e744699448559
Parth18 <48610764+Mazrah18@users.noreply.github.com>
2023-08-11 20:51:04 -0700

    final changes

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -9,11 +9,14 @@ const mongoURI = 'mongodb+srv://admin:12344321@blog.sjd6eaj.mongodb.net/?retryWr
 
 const client = new MongoClient(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true });
 
-app.use(cors({
-  origin: 'https://parth-blognest.netlify.app/',
-  methods: ['POST', 'GET'],
-  credentials: true,
-}));
+app.use(cors(
+  {
+    origin: ["https://cosmic-tarsier-7d078d.netlify.app", "https://parth-blognest.netlify.app"],
+    methods: ["POST", "GET"],
+    credentials: true
+  }
+));
+
 
 
 client.connect()
