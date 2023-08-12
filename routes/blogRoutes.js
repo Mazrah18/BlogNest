138938e022db616fe4308dff21e756cea1251345
Parth18 <48610764+Mazrah18@users.noreply.github.com>
2023-08-11 21:05:58 -0700

    route changes

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -19,7 +19,7 @@ const client = new MongoClient(mongoURI, { useNewUrlParser: true, useUnifiedTopo
 app.use(cors(
   {
     origin: ["https://parth-blognest.netlify.app"],
-    methods: ["POST", "GET"],
+    methods: ["POST", "GET", "DELETE"],
     credentials: true
   }
 ));
