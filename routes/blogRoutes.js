38aa65570720594b2ea27976d759599503dbd817
Parth18 <48610764+Mazrah18@users.noreply.github.com>
2024-03-22 14:27:01 -0700

    Update blogRoutes.js

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -19,20 +19,20 @@ const client = new MongoClient(mongoURI, { useNewUrlParser: true, useUnifiedTopo
 // ));
 app.use(cors(
   {
-    origin: [ "http://localhost:3000/"],
+    origin: "*",
     methods: ["POST", "GET", "DELETE"],
     credentials: true
   }
 ));
 
 
-app.use(cors(
-  {
-    origin: ["https://parth-blognest.netlify.app"],
-    methods: ["POST", "GET", "DELETE"],
-    credentials: true
-  }
-));
+// app.use(cors(
+//   {
+//     origin: ["https://parth-blognest.netlify.app", "http://localhost:3000/"],
+//     methods: ["POST", "GET", "DELETE"],
+//     credentials: true
+//   }
+// ));
 
 
 
